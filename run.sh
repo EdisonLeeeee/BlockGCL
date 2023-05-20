6187937bc624d86f17ef1899ba5a1c3fccd5e180
EdisonLeeeee <cnljt@outlook.com>
2023-05-20 12:53:07 +0800

    uncomment scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-# python train.py --flagfile=./configs/cora.cfg --gpu_id=0
-# python train.py --flagfile=./configs/citeseer.cfg --gpu_id=0
-# python train.py --flagfile=./configs/pubmed.cfg --gpu_id=0
-# python train.py --flagfile=./configs/computers.cfg --gpu_id=0
-# python train.py --flagfile=./configs/photos.cfg --gpu_id=0
-# python train.py --flagfile=./configs/cs.cfg --gpu_id=0
-# python train.py --flagfile=./configs/physics.cfg --gpu_id=1
-# python train.py --flagfile=./configs/mag.cfg --gpu_id=1
+python train.py --flagfile=./configs/cora.cfg --gpu_id=0
+python train.py --flagfile=./configs/citeseer.cfg --gpu_id=0
+python train.py --flagfile=./configs/pubmed.cfg --gpu_id=0
+python train.py --flagfile=./configs/computers.cfg --gpu_id=0
+python train.py --flagfile=./configs/photos.cfg --gpu_id=0
+python train.py --flagfile=./configs/cs.cfg --gpu_id=0
+python train.py --flagfile=./configs/physics.cfg --gpu_id=1
+python train.py --flagfile=./configs/mag.cfg --gpu_id=1
